302eb9cd3009953f561eadc493a62edb1bb60660
vitindugrau1 <142263154+vitindugrau1@users.noreply.github.com>
2024-10-16 11:08:36 -0300

    dada

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criacartao(Categoria,pergunta,resposta){
+function criacartao(categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
