4fcdfc99099c1a49ab93eae01899e07e15db52fa
vitindugrau1 <142263154+vitindugrau1@users.noreply.github.com>
2024-11-05 07:58:31 -0300

    dadaa

--- projeto2.js
+++ projeto2.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, resposta) {
+function criacartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
