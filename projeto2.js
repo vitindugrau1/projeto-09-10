545d189435aace469ddcc3a214980eb71bbb90f6
vitindugrau1 <142263154+vitindugrau1@users.noreply.github.com>
2024-11-05 07:43:37 -0300

    dada

---
+++ projeto2.js
@@ -0,0 +1,27 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = ` 
+    <div class="conteudo-cartao" onclick ="mostraresposta(this)">
+        <h3>${categoria}</h3>
+        <div class="pergunta-cartao">
+            <p>${pergunta}</p>
+        </div>
+        <div class="resposta-cartao">
+            <p>${resposta}</p>
+        </div>
+    </div>
+    `
+    container.appendChild(cartao);
+}
+
+function mostraresposta(card) {
+    const resposta = card.querySelector('.resposta-cartao');
+    if (resposta.style.display === 'none') {
+        resposta.style.display = 'block';
+    }
+    else {
+        resposta.style.display = 'none';
+    }
+}
